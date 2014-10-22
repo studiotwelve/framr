4e5bc936363052ec5444657cc255de79c8eaf0f5
Michael Hurley <studio12designs@me.com>
2014-10-22 09:18:17 -0400

    merged twbs / deploy
    *moved .htaccess directives to httpd.conf

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<?php phpinfo(); ?>
+<?php
+date_default_timezone_set("America/New_York");
+include("inc/bootstrap.php");
--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
-
+if(navigator.userAgent.match(/IEMobile\/10\.0/)){var msViewportStyle=document.createElement('style')msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'))document.querySelector('head').appendChild(msViewportStyle)}//!fix viewport
+(function($){var nua=navigator.userAgent,isAndroid=(nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1);if(isAndroid){$('select.form-control').removeClass('form-control').css('width','100%')}})(jQuery)//!fix droid selects
